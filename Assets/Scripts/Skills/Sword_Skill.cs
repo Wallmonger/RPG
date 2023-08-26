365c47c21ad1e390dc3b0eeb19236e56c43191fc
Wallmonger <nicolas.berrier360@gmail.com>
2023-08-26 21:00:21 +0200

    Retrieving mouse position / Getting final value of throwing direction

--- Assets/Scripts/Skills/Sword_Skill.cs
+++ Assets/Scripts/Skills/Sword_Skill.cs
@@ -6,8 +6,18 @@ public class Sword_Skill : Skill
 {
     [Header("Skill info")]
     [SerializeField] private GameObject swordPrefab;
-    [SerializeField] private Vector2 launchDir;
+    [SerializeField] private Vector2 launchForce;
     [SerializeField] private float swordGravity;
+
+    // will be used to calculate the ending direction of the sword, based on gravity
+    private Vector2 finalDir;
+
+    protected override void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+            // Getting the values of direction between 0 and 1, then multiply by the force of each axis
+            finalDir = new Vector2(AimDirection().normalized.x * launchForce.x, AimDirection().normalized.y * launchForce.y);
+    }
 
     public void CreateSword()
     {
@@ -17,6 +27,22 @@ public class Sword_Skill : Skill
         // Getting access of Sword_Skill_Controller via created prefab
         Sword_Skill_Controller newSwordScript = newSword.GetComponent<Sword_Skill_Controller>();
 
-        newSwordScript.SetupSword(launchDir, swordGravity);
+        newSwordScript.SetupSword(finalDir, swordGravity);
     }
+
+    // Will return a vector2 value of the mouse aiming
+    public Vector2 AimDirection()
+    {
+        Vector2 playerPosition = player.transform.position;
+
+        // Get the mouse position based on the screen ( x-axis / y-axis )
+        // ScreenToWorldPoint takes an object to check its position based on the screen
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Getting the direction of the sword's throwing
+        Vector2 direction = mousePosition - playerPosition;
+
+        return direction;
+    }
+
 }
