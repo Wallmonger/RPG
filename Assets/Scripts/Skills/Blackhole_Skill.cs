b0d673faf5a6ae8582fb10686ce538a39ceddb92
Wallmonger <nicolas.berrier360@gmail.com>
2023-09-26 16:57:31 +0200

    BlackHole_Skill

--- Assets/Scripts/Skills/Blackhole_Skill.cs
+++ Assets/Scripts/Skills/Blackhole_Skill.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blackhole_Skill : MonoBehaviour
+public class Blackhole_Skill : Skill
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int amountOfAttacks;
+    [SerializeField] private float cloneCooldown;
+    [Space]
+    [SerializeField] private GameObject blackHolePrefab;
+    [SerializeField] private float maxSize;
+    [SerializeField] private float growSpeed;
+    [SerializeField] private float shrinkSpeed;
+
+
+    public override bool CanUseSkill()
     {
+        return base.CanUseSkill();
+    }
+
+    public override void UseSkill()
+    {
+        base.UseSkill();
+
+        GameObject newBlackHole = Instantiate(blackHolePrefab);
+
+        Blackhole_Skill_Controller newBlackHoleScript = newBlackHole.GetComponent<Blackhole_Skill_Controller>();
+
+        newBlackHoleScript.SetupBlackHole(maxSize, growSpeed, shrinkSpeed, amountOfAttacks, cloneCooldown);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
-        
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
     }
 }
