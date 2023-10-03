17855e6422c725de9cbddaa9f24040eb040afada
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-03 17:51:19 +0200

    currCrystal duration

--- Assets/Crystal_Skill.cs
+++ Assets/Crystal_Skill.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Crystal_Skill : Skill
 {
+    [SerializeField] private float crystalDuration;
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
 
@@ -14,7 +15,14 @@ public class Crystal_Skill : Skill
 
         // No crystal = create one. Else teleport to existing crystal then remove it
         if (currentCrystal == null)
+        {
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
+            Crystal_Skill_Controller currentCrystalScript = currentCrystal.GetComponent<Crystal_Skill_Controller>();
+
+            currentCrystalScript.SetupCrystal(crystalDuration);
+
+        }
+            
         else
         {
             player.transform.position = currentCrystal.transform.position;
--- Assets/Scripts/Skills/Skill_Controllers/Crystal_Skill_Controller.cs
+++ Assets/Scripts/Skills/Skill_Controllers/Crystal_Skill_Controller.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Crystal_Skill_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float crystalExistTimer;
+
+
+    public void SetupCrystal(float _crystalDuration)
     {
-        
+        crystalExistTimer = _crystalDuration;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        crystalExistTimer -= Time.deltaTime;    
+
+        if (crystalExistTimer < 0)
+            SelfDestroy();
+            
     }
+
+    public void SelfDestroy() => Destroy(gameObject);
 }
