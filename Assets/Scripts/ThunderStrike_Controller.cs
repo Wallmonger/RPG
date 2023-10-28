2e0d35f574200ec044f2b48e3fe080bb1e24565c
Wallmonger <nicolas.berrier360@gmail.com>
2023-10-28 20:05:19 +0200

    Prefab / Fix rotation

--- Assets/Scripts/ThunderStrike_Controller.cs
+++ Assets/Scripts/ThunderStrike_Controller.cs
@@ -6,8 +6,10 @@ public class ThunderStrike_Controller : MonoBehaviour
 {
     [SerializeField] private CharacterStats targetStats;
     [SerializeField] private float speed;
+   
 
     private Animator anim;
+    private bool triggered;
     
     void Start()
     {
@@ -15,17 +17,37 @@ public class ThunderStrike_Controller : MonoBehaviour
     }
 
     
+    
     void Update()
     {
+        if (triggered)
+            return;
+
         // Origin, Target, Time
         transform.position = Vector2.MoveTowards(transform.position, targetStats.transform.position, speed * Time.deltaTime);
+
+        // Rotating lightning bolt to face enemy
+        transform.right = transform.position - targetStats.transform.position;
 
         // If lightning close to enemy, deal damage, then delete
         if (Vector2.Distance(transform.position, targetStats.transform.position) < .1f)
         {
-            targetStats.TakeDamage(1);
+            // Return original rotation of anim, and make it 3x bigger on impact
+            anim.transform.localPosition = new Vector3(0, .5f);  // Fix ground offset
+            anim.transform.localRotation = Quaternion.identity;
+
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = new Vector3(3,3);
+
+            Invoke("DamageAndSelfDestroy", .2f);
+            triggered = true;
             anim.SetTrigger("Hit");
-            Destroy(gameObject, .4f);
         }
+    }
+
+    private void DamageAndSelfDestroy()
+    {
+            targetStats.TakeDamage(1);
+            Destroy(gameObject, .4f);
     }
 }
