3d29be1ba4abb5208d4968de92b3bb012d0723f1
Wallmonger <nicolas.berrier360@gmail.com>
2023-11-04 19:44:18 +0100

    Equipment Types

--- Assets/Scripts/Items and Inventory/ItemData.cs
+++ Assets/Scripts/Items and Inventory/ItemData.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 
-
+public enum ItemType
+{
+    Material,
+    Equipment
+}
 //! menuName adds a create asset menu on right click
 /*[CreateAssetMenu(fileName ="default file name on creation", menuName ="path to create the asset on unity editor")]*/
-[CreateAssetMenu(fileName = "Something", menuName="Data/Item")]
+[CreateAssetMenu(fileName = "New Item Data", menuName="Data/Item")]
 
 
 //! Scriptable Object can be created in editor and can store data (like prefabs but any data not just GameObject)
 public class ItemData : ScriptableObject
 {
+    public ItemType itemType;
     public string itemName;
     public Sprite icon;
 }
