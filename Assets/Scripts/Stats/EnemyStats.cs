8f6ba748ad374e3b7338eefbbc559fc68866e5ef
Wallmonger <nicolas.berrier360@gmail.com>
2023-11-25 18:35:04 +0100

    Levels for enemies

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -6,11 +6,54 @@ public class EnemyStats : CharacterStats
 {
     private Enemy enemy;
 
+    [Header("Level details")]
+    [SerializeField] private int level = 1;
+
+    // Create a range input field linked to the variable below
+    [Range(0f, 1f)]
+    [SerializeField] private float percentageModifier = .4f;
+
     protected override void Start()
     {
+        ApplyLevelModifiers();
         base.Start();
 
-        enemy = GetComponent<Enemy>();  
+        enemy = GetComponent<Enemy>();
+
+    }
+
+    private void ApplyLevelModifiers()
+    {
+        Modify(strength);
+        Modify(agility);
+        Modify(intelligence);
+        Modify(vitality);
+
+        Modify(damage);
+        Modify(critChance);
+        Modify(critPower);
+
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(evasion);
+        Modify(magicResistance);
+
+        Modify(fireDamage);
+        Modify(iceDamage);
+        Modify(lightningDamage);
+    }
+
+
+    // Add bonus stats depending of the level
+    private void Modify(Stat _stat)
+    {
+        // Only increase stats if level bigger than 1
+        for (int i = 1; i < level; i++)
+        {
+            float modifier = _stat.GetValue() * percentageModifier;
+
+            _stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int _damage)
