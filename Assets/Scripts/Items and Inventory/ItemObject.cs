f341c2d1a9cf2729e3c50645729b66163b237b0b
Wallmonger <nicolas.berrier360@gmail.com>
2023-12-06 18:04:09 +0100

    ItemObject velocity

--- Assets/Scripts/Items and Inventory/ItemObject.cs
+++ Assets/Scripts/Items and Inventory/ItemObject.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class ItemObject : MonoBehaviour
 {
-
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private ItemData itemData;
+    [SerializeField] private Vector2 velocity;
 
     // Called when the script is loaded or value change in the inspector
     private void OnValidate()
     {
+        // In case itemData is not set
+        if (itemData == null)
+            return;
+
         GetComponent<SpriteRenderer>().sprite = itemData.icon;
         gameObject.name = $"Item object - {itemData.itemName}";
     }
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
     {
-        if (collision.GetComponent<Player>() != null)
-        {
-            Debug.Log("Picked up item " + itemData.itemName);
-            Inventory.instance.AddItem(itemData);
-            Destroy(gameObject);
-        }  
+        if (Input.GetKeyDown(KeyCode.M))
+            rb.velocity = velocity;
     }
 
+    public void SetupItem(ItemData _itemData, Vector2 _velocity)
+    {
+        itemData = _itemData;
+        rb.velocity = _velocity;
+    }
+
+    public void PickupItem()
+    {
+        Inventory.instance.AddItem(itemData);
+        Destroy(gameObject);
+    }
 }
---
+++ Assets/Scripts/Items and Inventory/ItemObject_Trigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject_Trigger : MonoBehaviour
+{
+    private ItemObject myItemObject => GetComponentInParent<ItemObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Debug.Log("Item Picked Up");
+            myItemObject.PickupItem();
+        }
+    }
+}
